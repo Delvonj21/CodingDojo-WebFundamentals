211d3b66fe88d0e473b7fbfa698a80a1f99d782a
Delvon Johnson <delvonj21@gmail.com>
2023-08-15 23:09:06 +0200

    added assignment

---
+++ Assignments/JavaScript/DojoWeather/script.js
@@ -0,0 +1,15 @@
+var cities = document.querySelector("#cities");
+
+
+
+function displayWeatherReport() {
+  alert("Loading weather report...");
+}
+
+function acceptCookie() {
+  var cookieBox = document.getElementById('cookie');
+  cookieBox.style.display = 'none';
+}
+
+
+
